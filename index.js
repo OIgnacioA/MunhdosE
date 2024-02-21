99cb73d6b397179ce65f0686de86c5ad16b36112
avendaño Oscar Ignacio <sehent@hotmail.com>
2024-02-20 21:07:11 -0300

    primer intento

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = new Koa();
 const router = new Router();
 const sum = require('./sum');
 
+
+// Nueva ruta para la ruta raíz "/"
+router.get('/', (ctx, next) => {
+  ctx.body = 'Welcome to the application!----Ejemplo de url a usar: http://localhost:3000/add/3/5';
+});
+
+
+
 router.get('/add/:a/:b', (ctx, next) => {
   const result = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
   return ctx.body = { result };
